37941e6f29d415c47d35f21d8a4267dca24a3157
Michael <MTriantafelow@gmail.com>
2019-04-28 17:42:38 -0400

    Accept 'scan' commands

--- paper_trail.py
+++ paper_trail.py
@@ -1,7 +1,12 @@
+import sys
 import os
 
-for path in os.listdir():
-    parts = path.split()
-    date = parts[0]
-    if (len(date) != 10):
-        print('Date Warning: ' + path)
+command = sys.argv[1]
+if command == 'scan':
+    print('Invalid Date(s)')
+    print('---------------')
+    for path in os.listdir():
+        parts = path.split()
+        date = parts[0]
+        if (len(date) != 10):
+            print(path)
